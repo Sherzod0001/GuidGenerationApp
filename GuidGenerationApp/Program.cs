460b6482f9028cfa35149ce4097edebaf2e02fa2
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-06-27 22:35:45 +0500

    Add project files.

---
+++ GuidGenerationApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuidGenerationApp
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+            string yesOrNo = "";
+            do 
+            {
+                string StringGenerationGuid = Guid.NewGuid().ToString();
+
+                Console.WriteLine($"New Guid: {StringGenerationGuid}");
+                TextCopy.ClipboardService.SetText(StringGenerationGuid);
+                Console.WriteLine("Copied....\n");
+                
+                Console.WriteLine("Do you want to continue? (yes / no)");
+                yesOrNo = Console.ReadLine();
+                Console.WriteLine();
+            } while (yesOrNo.ToLower() == "yes" | yesOrNo.ToLower() == "y");
+            
+
+       }
+    }
+}
